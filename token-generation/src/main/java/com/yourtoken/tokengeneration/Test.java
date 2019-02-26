2f9c854e6c27293e8dd1585c1cc8497c2c458be8
BDII1335 <BDII1335@BDII.COM>
2019-02-26 21:55:46 +0530

    testing1

---
+++ token-generation/src/main/java/com/yourtoken/tokengeneration/Test.java
@@ -0,0 +1,14 @@
+package com.yourtoken.tokengeneration;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void test()
+	{
+		System.out.println("hello");
+	}
+}
